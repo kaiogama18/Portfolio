5552d72d14547a23c93b2bb859238b9557a43ddb
Kaio B. Gama <kaioeduardoescar@gmail.com>
2021-01-23 20:10:26 -0400

    metadata
    Signed-off-by: Kaio B. Gama <kaioeduardoescar@gmail.com>

---
+++ config/metadata.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: `KaioGama`,
+    siteUrl: `https://www.kaiogama.com`,
+    description: `my personal website portfolio`,
+    social: [
+        {
+            name: 'github',
+            url: 'https://github.com/kaiogama18'
+        },
+        {
+            name: 'linkedin',
+            url: 'https://www.linkedin.com/in/kaiogama/'
+        }
+    ],
+    footer: [
+        {
+            copyright: 'Copyright 2021 Kaio B. Gama - Todos os direitos reservados'
+        }
+    ]
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+const siteMetadata = require('./config/metadata')
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata,
   plugins: [],
 }
