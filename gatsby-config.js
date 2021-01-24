11851818722aaae9f744836adc77a93c64ce8227
Kaio B. Gama <kaioeduardoescar@gmail.com>
2021-01-23 21:42:26 -0400

    add plugins
    Signed-off-by: Kaio B. Gama <kaioeduardoescar@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,66 @@ const siteMetadata = require('./config/metadata')
 
 module.exports = {
   siteMetadata,
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `black`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Kaio Gama`,
+        short_name: `K.Gama`,
+        description: `My personal website portfolio`,
+        lang: `pt-br`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#525659`, //#ff954b
+        display: `standalone`,
+        icon: 'src/images/icon.png' //512x512
+      },
+      icon_options: {
+        purpose: `any maskable`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://www.kaiogama.com`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV != 'production'
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-netlify-cache",
+      options: {
+        cachePublic: true
+      }
+    },
+    `gatsby-plugin-offline`
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Meu site Kaio Gama</div>
+  return (
+    <div>
+      <h3>TRABALHO COM DESENVOLVIMENTO WEB E MOBILE DESDE 2015, DESENVOLVI
+      VÁRIOS APLICATIVOS. SEMPRE PREOCUPADO COM A EXPERIÊNCIA DO USUÁRIO E COM O
+USO DE DADOS MÉTRICAS PARA ALCANÇAR ALTAS TAXAS DE RETENÇÃO.</h3>
+      <p>
+        Experiência
+    </p>
+    </div>
+  )
 }
