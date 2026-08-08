fd5fd00cb111357c080a96ef924fd5efc7a1380f
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2026-08-08 02:23:36 -0400

    [add] - footer bar

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <section id="spacer"></section>
+            <div className='footer-links'>
+                <a href="#home">Início</a>
+                <a href="#about">Sobre</a>
+                <a href="#contact">Contato</a>
+            </div>
+
+            <p>@ 2026 by Kaio Gama</p>
+        </footer>
+    )
+}
+
+export default Footer
